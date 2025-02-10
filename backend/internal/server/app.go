4b244b8e397c1698bec92a5680542af6d5d5e6d1
brahem <moroccobrahem@gmail.com>
2025-02-10 11:44:35 -0500

    get ignore and some handling in app

--- backend/internal/server/app.go
+++ backend/internal/server/app.go
@@ -13,7 +13,7 @@ func Run(cfg *config.Conf)  {
 	// Prepare logger
 	file, err := os.OpenFile("logfile.log", os.O_APPEND|os.O_RDWR|os.O_CREATE, 0o644)
 	if err != nil {
-		log.Printf("cannot create log file: %v", err)
+		log.Panic("cannot create log file: ", err)
 	}
 	defer file.Close()
 	logWriter := io.MultiWriter(file, os.Stdout)
