56ca1a79ee472b3e14f68f6a10ed7a4e3cbbcca9
obenali <oussama.benali@ump.ac.ma>
2025-03-11 17:28:52 +0000

    testing ping :)

--- backend/internal/server/app.go
+++ backend/internal/server/app.go
@@ -1,13 +1,18 @@
 package server
 
 import (
+	"testing"
+	"time"
+
 	"socialNetwork/internal/chat"
 	"socialNetwork/internal/comment"
 	"socialNetwork/internal/group"
 	"socialNetwork/internal/user"
 	"socialNetwork/pkg/config"
+	"socialNetwork/pkg/database"
 	"socialNetwork/pkg/loger"
 	scs "socialNetwork/pkg/sessions"
+	"socialNetwork/pkg/websocket"
 )
 
 type App struct {
@@ -28,4 +33,43 @@ func NewApp(dep *config.Dependencies) *App {
 		Group:          group.NewGroup(dep /* we need to add the hub to group*/),
 		User:           user.NewUser(dep /* we need to add the hub*/),
 	}
+}
+
+func NewTestApplication(t *testing.T) (*App, *config.Conf) {
+	loger := loger.NewLogger()
+	// And a form decoder.
+	// formDecoder := form.NewDecoder()
+	// And a session manager instance. Note that we use the same settings as
+	// production, except that we *don't* set a Store for the session manager.
+	// If no store is set, the SCS package will default to using a transient
+	// in-memory store, which is ideal for testing purposes.
+	sessionManager := scs.New()
+	sessionManager.Lifetime = 12 * time.Hour
+	sessionManager.Cookie.Secure = true
+
+	cfg, err := config.NewConfig()
+	if err != nil {
+		loger.Error.Panicln("error while reading config file\n", err)
+	}
+
+	db, err := database.InitDB(cfg.Database)
+	if err != nil {
+		loger.Error.Panicf("error occured while connecting database: %s", err.Error())
+	}
+	defer db.Close()
+
+	dep := &config.Dependencies{
+		SessionManager: sessionManager,
+		DB:             db,
+		Loger:          loger,
+		/*Legislation is the process or result of enrolling, enacting, or promulgating laws by a legislature, parliament, or analogous governing body.*/
+		//hub := websocket.NewHub() // need console legislation
+		Hub: websocket.NewHub(),
+	}
+
+	return &App{
+		SessionManager: sessionManager,
+		Loger:          loger,
+		User:           user.NewUser(dep /* we need to add the hub*/),
+	}, cfg
 }
