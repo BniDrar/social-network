470fce2be2186d6e9133bcbd397d13e7bb8cb696
oaitbenh <0xoabh@gmail.com>
2025-03-11 05:46:33 +0000

    posts methos without sql queries

---
+++ backend/entity/post.go
@@ -0,0 +1,15 @@
+package entity
+
+import "time"
+
+type Post struct {
+	ID        int       `json:"id"`
+	Title     string    `json:"title"`
+	Image     []byte    `json:"image"`
+	Content   string    `json:"content"`
+	UserID    int       `json:"user_id"`
+	Status    int       `json:"status"` // 0: private, 1: friends, 2: global
+	GroupID   int       `json:"group"`  // Group ID, can be null
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
--- backend/internal/post/handler.go
+++ backend/internal/post/handler.go
@@ -2,8 +2,10 @@ package post
 
 import (
 	"database/sql"
+	"encoding/json"
 	"net/http"
 
+	"socialNetwork/entity"
 	"socialNetwork/pkg/config"
 	"socialNetwork/pkg/loger"
 	"socialNetwork/pkg/websocket"
@@ -21,25 +23,106 @@ type post struct {
 	loger loger.CstmLogger
 }
 
+var _ string = `{
+					"id": 1,
+					"title": "My First Post",
+					"image": "iVBORw0KGgoAAAANSUhEUgAA...kJggg==",
+					"content": "This is the content of the post.",
+					"user_id": 42,
+					"status": 1,
+					"group": 5,
+					"created_at": "2025-03-11T12:34:56Z",
+					"updated_at": "2025-03-11T12:34:56Z"
+				}`
+
 func Newpost(dep *config.Dependencies) Post {
 	return &post{db: dep.DB, loger: *dep.Loger, Hub: dep.Hub}
 }
 
-func (u *post) GetPosts(w http.ResponseWriter, r *http.Request) {}
+func (u *post) GetPosts(w http.ResponseWriter, r *http.Request) {
+	id := r.Context().Value(entity.ContextID).(int)
+	prep, err := u.db.PrepareContext(r.Context(), ``)
+	if err != nil {
+		return
+	}
+	posts := []entity.Post{}
+	res, err := prep.QueryContext(r.Context(), id)
+	if err != nil {
+		return
+	}
+	for res.Next() {
+		post := entity.Post{}
+		err := res.Scan(&post.ID, &post.UserID, &post.Title, &post.Content, &post.Image, &post.GroupID)
+		if err != nil {
+			continue
+		}
+		posts = append(posts, post)
+	}
+	data, err := json.Marshal(posts)
+	if err != nil {
+		return
+	}
+	w.Write(data)
+}
 
-func (u *post) React(w http.ResponseWriter, r *http.Request) {}
+func (u *post) React(w http.ResponseWriter, r *http.Request) {
+	id := r.Context().Value(entity.ContextID).(int)
+	prep, err := u.db.PrepareContext(r.Context(), ``)
+	if err != nil {
+		return
+	}
+	_, err = prep.Exec(id)
+	if err != nil {
+		return
+	}
+	w.Write([]byte(`{
+		result: "done"
+	}`))
+}
 
 func (u *post) Post(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
-	case "Post":
+	case "POST":
 		u.CreatePost(w, r)
-	case "Get":
+	case "GET":
 		u.GetPost(w, r)
 	default:
 		w.WriteHeader(http.StatusMethodNotAllowed)
 	}
 }
 
-func (u *post) CreatePost(w http.ResponseWriter, r *http.Request) {}
+func (u *post) CreatePost(w http.ResponseWriter, r *http.Request) {
+	id := r.Context().Value(entity.ContextID).(int)
+	prep, err := u.db.PrepareContext(r.Context(), ``)
+	if err != nil {
+		return
+	}
+	post := entity.Post{}
+	json.NewDecoder(r.Body).Decode(&post)
+	_, err = prep.Exec(id, post.Title, post.Content, post.Image, post.GroupID, post.Status)
+	if err != nil {
+		return
+	}
+	w.Write([]byte(`{
+		result: "done"
+	}`))
+}
 
-func (u *post) GetPost(w http.ResponseWriter, r *http.Request) {}
+func (u *post) GetPost(w http.ResponseWriter, r *http.Request) {
+	id := r.Context().Value(entity.ContextID).(int)
+	prep, err := u.db.PrepareContext(r.Context(), ``)
+	if err != nil {
+		return
+	}
+	post := entity.Post{}
+	res := prep.QueryRowContext(r.Context(), id)
+	err = res.Scan(&post.ID, &post.Title, &post.Content, &post.Image, &post.Status, &post.GroupID)
+	if err != nil {
+		return
+	}
+	data, err := json.Marshal(post)
+	if err != nil {
+		return
+	}
+	w.Write(data)
+}
