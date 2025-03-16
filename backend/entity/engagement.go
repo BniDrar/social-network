f37445427dcf3a9b8e26b219ccc989f4fc39e525
melfihry <fihrylara@gmail.com>
2025-03-16 23:16:26 +0000

    clearn the engagement entity file

--- backend/entity/engagement.go
+++ backend/entity/engagement.go
@@ -1,20 +1,5 @@
 package entity
 
-/*
-CREATE TABLE IF NOT EXISTS engagement (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    comment_id INTEGER,
-    post_id INTEGER,
-    event_id INTEGER,
-    user_id INTEGER,
-    status INTEGER, -- 0: none, 1: like, 2: dislike
-    FOREIGN KEY (comment_id) REFERENCES comments(id) ON DELETE CASCADE,
-    FOREIGN KEY (post_id) REFERENCES posts(id) ON DELETE CASCADE,
-    FOREIGN KEY (event_id) REFERENCES events(id) ON DELETE CASCADE,
-    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
-);
-*/
-
 type Engagement struct {
 	ID        int    `json:"id"`
 	CommentID int    `json:"comment_id"`
