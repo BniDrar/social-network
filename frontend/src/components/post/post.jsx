601418506d5dd944024e6d88028a99fe8f4cb054
yrahhaou <yassinerahhaoui12@gmail.com>
2025-03-19 12:50:05 +0000

    add post component

--- frontend/src/components/post/post.jsx
+++ frontend/src/components/post/post.jsx
@@ -5,12 +5,24 @@ const Post = () => {
         <article className={styles.card}>
             <div className={styles.card_header}>
                 {/* avatar - username - creation_date */}
+                <img src="" className={styles.post_avatar} alt="avatar" title='avatar' />
+                <div className="">
+                    <h4 className={styles.post_username}></h4>
+                    <span className={styles.post_created_at}></span>
+                </div>
             </div>
             <div className={styles.card_body}>
                 {/* text - image - reactions_len - comments_len */}
+                <p className={styles.post_text}></p>
+                <img src="" className={styles.post_image} alt="post image" />
+                <div className="">
+                    <div className={styles.post_reactions_len}></div>
+                    <div className={styles.post_comments_len}></div>
+                </div>
             </div>
             <div className={styles.card_footer}>
                 {/* add_reaction - add_comment */}
+
             </div>
         </article>
     );
