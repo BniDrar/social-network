966644f94991aa0f606b983230b4b4f547a17d7f
yrahhaou <yassinerahhaoui12@gmail.com>
2025-03-29 16:26:20 +0000

    update NewPostCard component

--- frontend/src/components/NewPostCard/NewPostCard.jsx
+++ frontend/src/components/NewPostCard/NewPostCard.jsx
@@ -1,15 +1,20 @@
 import styles from './NewPostCard.module.css'
+import avatar from '@/assets/images/no-face.jpg'
+import Image from 'next/image';
 
 const NewPostCard = () => {
     return (
         <div className={styles.card}>
             <div className={styles.cardHeader}>
-                {/* <Image className={styles.createPostAvatar} src={avatar} width={150} height={150} alt="" /> */}
-                <form method="post">
+                <Image className={styles.createPostAvatar} src={avatar} width={150} height={150} alt="" />
+                <form method="post" className={styles.form}>
                     <input type="text" name="" placeholder="What's on your mind..." className={styles.createPost} />
                 </form>
             </div>
-            <div className={styles.cardHeader}></div>
+            <div className={styles.cardBody}>
+                <button className="">Create Post</button>
+                <button className="">Create Event</button>
+            </div>
         </div>
     );
 }
