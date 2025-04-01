6fd9587702bc567a547c0e3b7ef031667d9b6eb1
yrahhaou <yassinerahhaoui12@gmail.com>
2025-04-01 01:15:49 +0000

    update ProfileFriends component

--- frontend/src/app/page.jsx
+++ frontend/src/app/page.jsx
@@ -1,11 +1,14 @@
 import PostList from "@/components/PostList/PostList";
+import styles from "./page.module.css"
 
 export default function Home() {
   return (
     <div>
-     <main>
-      <PostList />
-     </main>
+      <main className={styles.container}>
+        <div className={styles.postsContainer}>
+          <PostList />
+        </div>
+      </main>
     </div>
   );
 }
