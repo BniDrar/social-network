71495ab82559f32c5156a3fc6432e15e4b19bb50
OUSSAMABENALI <oussama.benali@ump.ac.ma>
2025-04-13 22:49:05 +0100

    remove debuging lines from WsListing

--- backend/internal/chat/service.go
+++ backend/internal/chat/service.go
@@ -4,7 +4,6 @@ import (
 	"bytes"
 	"context"
 	"errors"
-	"fmt"
 	"log"
 	"socialNetwork/entity"
 	ws "socialNetwork/pkg/websocket"
@@ -14,30 +13,19 @@ import (
 
 func WsListing(conn *websocket.Conn, ctx context.Context) error {
 	defer conn.Close()
-	fmt.Println("in ws listing")
 	m := ws.NewManager()
-	fmt.Println("new manager:", m)
-	fmt.Println("before context")
 	val := ctx.Value(entity.ContextID)
 	idInt, ok := val.(int)
-	fmt.Println("val:", val)
-	fmt.Printf("val: %v, type: %T\n", val, val)
-	fmt.Println("ok", ok)
-	fmt.Println("id", idInt)
 	id := uint(idInt) // convert after
 	if !ok {
-		fmt.Println("error in context")
 		// handle the error: not found or wrong type
 		return errors.New("user ID missing or invalid in context")
 	}
-	fmt.Println("after context")
-	fmt.Println("id in listing", id)
 	m.AddClient(id, conn)
 	defer m.RemoveClient(id)
 	log.Printf("Client %d connected", id)
 	// Listen for messages from the client
 	for {
-		fmt.Println("in the loop reading messages")
 		_, message, err := conn.ReadMessage()
 
 		if err != nil {
