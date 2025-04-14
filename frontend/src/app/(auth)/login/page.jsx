73442daa6b5d1159215bc9ad04aeec2b6e5c6051
oaitbenh <0xoabh@gmail.com>
2025-04-14 02:50:42 +0100

    login page

--- frontend/src/app/(auth)/login/page.jsx
+++ frontend/src/app/(auth)/login/page.jsx
@@ -1,15 +1,46 @@
-import React from 'react'
+'use client'
+
+import React, { useEffect, useState } from 'react'
+import styles from './page.module.css'
 
 export default function page() {
-    //logic
+  const [formData, setFormData] = useState({
+    username: "",
+    password: "",
+  })
+
+  const handleChange = (e) => {
+    const { id, value } = e.target;
+    console.log("id : ", id);
+    console.log("value : ", value);
+    
+    
+    setFormData(prevData => ({
+      ...prevData,
+      [id]: value
+    }));
+  }
+
+  const handleSubmit = () => {
+    useEffect(() => {
+      console.log(formData);
+      // const response = await fetch("/api/Auth", {
+      // body: formData,
+      // });
+      // const JsonRes = await response.json();
+      // if (!JsonRes.error) {
+      // location.href = "/";
+      // }
+    })
+  }
+
   return (
-    //view
-    <div>
-      <h1>zellcode</h1>
-      <form>
-        <input id='username' placeholder='enter email or username' required/>
-        <input type='password' placeholder='enter your password' required/> 
-        <button type = 'submit'>Login</button>
+    <div className={styles.container}>
+      <h1 className={styles.bigTitle}>zellcode</h1>
+      <form className={styles.form} onSubmit={handleSubmit}>
+        <input className={styles.input} id='username' placeholder='enter email or username' required onChange={handleChange} />
+        <input className={styles.input} type='password' placeholder='enter your password' required onChange={handleChange} />
+        <button className={styles.button} type='submit'>Login</button>
       </form>
     </div>
   )
