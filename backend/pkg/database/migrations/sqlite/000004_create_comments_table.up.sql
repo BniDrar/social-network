6dbf1a3a3c73efe9ee298a5534e4751037eef675
ielharra <moroccobrahem@gmail.com>
2025-04-17 18:49:40 +0100

    add the image to the comment table

--- backend/pkg/database/migrations/sqlite/000004_create_comments_table.up.sql
+++ backend/pkg/database/migrations/sqlite/000004_create_comments_table.up.sql
@@ -3,6 +3,7 @@ CREATE TABLE IF NOT EXISTS comments (
     post_id INTEGER,
     user_id INTEGER,
     content TEXT,
+    image TEXT,
     FOREIGN KEY (post_id) REFERENCES posts(id) ON DELETE CASCADE,
     FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
 );
