7dba2268e913b43f65afcb7f6cde726c8f45faed
OUSSAMABENALI <oussama.benali@ump.ac.ma>
2025-04-18 22:20:50 +0100

    working on sidebar

--- frontend/src/app/layout.jsx
+++ frontend/src/app/layout.jsx
@@ -2,6 +2,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navebar from "@/components/Navebar/Navebar";
+import SideBar from "@/components/SideBar/SideBar";
 import { GlobalProvider } from "@/contexts/GlobalContext";
 import Overlay from "@/components/Overlay/Overlay";
 
@@ -27,7 +28,8 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         <GlobalProvider>
-          <Navebar /> 
+          <Navebar />
+          <SideBar /> 
           <Overlay />
           {children}
         </GlobalProvider>
