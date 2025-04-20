192bbfade79c1a9c20a8de7dff9d41aa41f6b216
yrahhaou <yassinerahhhaoui12@gmail.com>
2025-04-20 16:41:39 +0100

    merge

--- frontend/src/app/layout.jsx
+++ frontend/src/app/layout.jsx
@@ -28,11 +28,8 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         <GlobalProvider>
-<<<<<<< HEAD
-=======
           <Navebar />
           {/* <SideBar />  */}
->>>>>>> yrahhaou
           <Overlay />
           {children}
         </GlobalProvider>
