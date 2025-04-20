6734bf6dac800e6605b9aa202768cf8fac0a753b
ielharra <moroccobrahem@gmail.com>
2025-04-20 14:29:16 +0100

    move the file upload function from the post services to the utils

--- backend/internal/post/service.go
+++ backend/internal/post/service.go
@@ -1,17 +1,11 @@
 package post
 
 import (
-	"bufio"
 	"context"
 	"encoding/json"
 	"errors"
-	"fmt"
 	"io"
-	"mime/multipart"
-	"net/http"
-	"os"
 	"strconv"
-	"strings"
 
 	"socialNetwork/entity"
 
@@ -86,48 +80,4 @@ func (p *post) GetPostsByUserService(ctx context.Context, username string) (data
 		return
 	}
 	return
-}
-
-func FileUpload(NeWFileName string, File multipart.File, FileHeader *multipart.FileHeader, err error) error {
-	if err != nil {
-		return err
-	}
-	fmt.Println((FileHeader.Size / 1024))
-	var FileContent []byte
-	var FileIsAccepted bool
-	AcceptedTypes := []string{"png", "gif", "jpg", "jpeg"}
-	for _, Type := range AcceptedTypes {
-		if strings.HasSuffix(FileHeader.Filename, Type) {
-			FileIsAccepted = true
-		}
-	}
-	if !FileIsAccepted {
-		return errors.New("file type is not accepted")
-	}
-	switch FileHeader.Filename[len(FileHeader.Filename)-3:] {
-	case "gif":
-		if (FileHeader.Size/1024) > 1024 || (FileHeader.Size/1024) < 10 {
-			return errors.New("file size unmatched our conditions")
-		}
-	default:
-		if (FileHeader.Size/1024) > 500 || (FileHeader.Size/1024) < 10 {
-			return errors.New("file size unmatched our conditions")
-		}
-	}
-	FileIsAccepted = false
-	reader := bufio.NewReader(File)
-	FileContent, _ = io.ReadAll(reader)
-	AcceptedTypes = []string{"image/png", "image/jpg", "image/jpeg", "image/gif"}
-	FileType := http.DetectContentType((FileContent))
-	for _, Type := range AcceptedTypes {
-		if Type == FileType {
-			FileIsAccepted = true
-		}
-		fmt.Println(FileIsAccepted, " : ", Type, " == ", FileType)
-	}
-	if !FileIsAccepted {
-		return errors.New("file type not matched")
-	}
-	os.WriteFile("../"+NeWFileName+".png", FileContent, 0o444)
-	return nil
 }
--- backend/pkg/utils/post.go
+++ backend/pkg/utils/post.go
@@ -1,6 +1,13 @@
 package utils
 
 import (
+	"bufio"
+	"errors"
+	"fmt"
+	"io"
+	"mime/multipart"
+	"net/http"
+	"os"
 	"strings"
 	"unicode"
 
@@ -9,7 +16,7 @@ import (
 
 func CheckCommentsCredentials(comnt entity.Comment) bool {
 	return len(removeAllWhitespace(comnt.Content)) > 0 &&
-	len(removeAllWhitespace(comnt.Content)) < 200 &&
+		len(removeAllWhitespace(comnt.Content)) < 200 &&
 		comnt.PostID > 0
 }
 
@@ -20,4 +27,48 @@ func removeAllWhitespace(s string) string {
 		}
 		return r
 	}, s)
+}
+
+func FileUpload(NeWFileName string, File multipart.File, FileHeader *multipart.FileHeader, err error) error {
+	if err != nil {
+		return err
+	}
+	fmt.Println((FileHeader.Size / 1024))
+	var FileContent []byte
+	var FileIsAccepted bool
+	AcceptedTypes := []string{"png", "gif", "jpg", "jpeg"}
+	for _, Type := range AcceptedTypes {
+		if strings.HasSuffix(FileHeader.Filename, Type) {
+			FileIsAccepted = true
+		}
+	}
+	if !FileIsAccepted {
+		return errors.New("file type is not accepted")
+	}
+	switch FileHeader.Filename[len(FileHeader.Filename)-3:] {
+	case "gif":
+		if (FileHeader.Size/1024) > 1024 || (FileHeader.Size/1024) < 10 {
+			return errors.New("file size unmatched our conditions")
+		}
+	default:
+		if (FileHeader.Size/1024) > 500 || (FileHeader.Size/1024) < 10 {
+			return errors.New("file size unmatched our conditions")
+		}
+	}
+	FileIsAccepted = false
+	reader := bufio.NewReader(File)
+	FileContent, _ = io.ReadAll(reader)
+	AcceptedTypes = []string{"image/png", "image/jpg", "image/jpeg", "image/gif"}
+	FileType := http.DetectContentType((FileContent))
+	for _, Type := range AcceptedTypes {
+		if Type == FileType {
+			FileIsAccepted = true
+		}
+		fmt.Println(FileIsAccepted, " : ", Type, " == ", FileType)
+	}
+	if !FileIsAccepted {
+		return errors.New("file type not matched")
+	}
+	os.WriteFile("../"+NeWFileName+".png", FileContent, 0o444)
+	return nil
 }
