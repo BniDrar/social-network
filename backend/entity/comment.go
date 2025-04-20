ce607ad784442fcea5044b08abc870c24cd9ca42
ielharra <moroccobrahem@gmail.com>
2025-04-20 16:07:14 +0100

    add the name of the creature of the comment to the struct

--- backend/entity/comment.go
+++ backend/entity/comment.go
@@ -1,9 +1,10 @@
 package entity
 
 type Comment struct {
-	ID      int    `json:"id"`
-	PostID  int    `json:"post_id"`
-	UserID  int    `json:"user_id"`
-	Content string `json:"content"`
-	Image   string `json:"image"`
+	ID          int    `json:"id"`
+	CreaterName string `json:"creater_name"`
+	PostID      int    `json:"post_id"`
+	UserID      int    `json:"user_id"`
+	Content     string `json:"content"`
+	Image       string `json:"image"`
 }
