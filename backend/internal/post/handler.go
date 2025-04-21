0941232ab8cabc7d2007b58783e7f4386145480b
oaitbenh <0xmrrandom@gmail.com>
2025-04-21 11:38:45 +0100

    merge posts and dev in valid posts

--- backend/internal/post/handler.go
+++ backend/internal/post/handler.go
@@ -4,18 +4,21 @@ import (
 	"database/sql"
 	"encoding/json"
 	"net/http"
-	"strconv"
 
 	"socialNetwork/entity"
 	"socialNetwork/pkg/config"
 	"socialNetwork/pkg/loger"
 	"socialNetwork/pkg/websocket"
+
+	"github.com/google/uuid"
 )
 
 type Post interface {
-	Post(w http.ResponseWriter, r *http.Request)
 	GetPosts(w http.ResponseWriter, r *http.Request)
-	// React(w http.ResponseWriter, r *http.Request)
+	CreatePost(w http.ResponseWriter, r *http.Request)
+	GetPost(w http.ResponseWriter, r *http.Request)
+	ReactPost(w http.ResponseWriter, r *http.Request)
+	GetPostByUsername(w http.ResponseWriter, r *http.Request)
 }
 
 type post struct {
@@ -25,215 +28,72 @@ type post struct {
 }
 
 var _ string = `{
-					"id": 1,
-					"title": "My First Post",
-					"image": "iVBORw0KGgoAAAANSUhEUgAA...kJggg==",
-					"content": "This is the content of the post.",
-					"user_id": 42,
-					"status": 1,
-					"group": 5,
-					"created_at": "2025-03-11T12:34:56Z",
-					"updated_at": "2025-03-11T12:34:56Z"
-				}`
+  				  "id": "1",
+  				  "avatar": "/assets/images/no-face.jpg",
+  				  "username": "yrahhaou",
+  				  "content": "🍲🍖 Classic Homestyle Meatloaf with a Tangy Glaze 🍋✨",
+  				  "image": "/assets/images/post1.jpg",
+  				  "likes": "15k",
+  				  "comments": "5k",
+  				  "user_like": false,
+  				  "created_at": "7h"
+  				}`
 
 func Newpost(dep *config.Dependencies) Post {
 	return &post{db: dep.DB, loger: *dep.Loger, Hub: dep.Hub}
 }
 
-/*this is not working ... obenali made this comment*/
-/*func (u *post) GetPosts(w http.ResponseWriter, r *http.Request) {
-	prep, err := u.db.PrepareContext(r.Context(), `SELECT
-			post.id
-			post.user_id
-		    title,
-		    content,
-			post.image,
-			post.group_id,
-		    (SELECT nickname FROM users AS u WHERE post.user_id=u.id) AS creator
-		FROM
-		    posts AS post
-		LEFT JOIN groups AS "group" ON "group".id = post.group_id
-		LEFT JOIN group_members AS gm ON gm.member_id = $1 AND gm.group_id = "group".id
-		LEFT JOIN follows AS follow
-		    ON (follow.followed_id = post.user_id AND
-		        follow.follower_id = $1 AND
-		        post.status = 0 AND
-		        post.group_id IS NULL)
-		WHERE
-		    (post.group_id IS NOT NULL AND gm.member_id IS NOT NULL)
-		    OR
-		    (post.group_id IS NULL AND follow.follower_id = $1);`)
+func (p *post) GetPosts(w http.ResponseWriter, r *http.Request) {
+	data, err := p.Service_GetAll(r.Context())
 	if err != nil {
-		u.loger.Error.Println(err)
+		w.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(w).Encode(entity.ErrorResponse{Error: "Invalid request body"})
 		return
 	}
-	posts := []entity.Post{}
-	res, err := prep.QueryContext(r.Context())
-	if err != nil {
-		u.loger.Error.Println(err)
-		return
-	}
-	for res.Next() {
-		post := entity.Post{}
-		err := res.Scan(&post.ID, &post.UserID, &post.Title, &post.Content, &post.Image, &post.GroupID, &post.Nickname)
-		if err != nil {
-			u.loger.Error.Println(err)
-			continue
-		}
-		posts = append(posts, post)
-	}
-	data, err := json.Marshal(posts)
-	if err != nil {
-		u.loger.Error.Println(err)
-		return
-	}
-
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-
 	w.Write(data)
 }
-*/
 
-func (p *post) GetPosts(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodGet {
-		http.Error(w, "method not allowed", http.StatusMethodNotAllowed)
-		return
-	}
-	p.loger.Info.Println(r.URL)
-	// get the limit and offset from the request body
-	limitStr := r.URL.Query().Get("limit")
-	p.loger.Info.Println("limit:", limitStr)
-	offsetStr := r.URL.Query().Get("offset")
-	p.loger.Info.Println("offset:", offsetStr)
-
-	limit, err := strconv.Atoi(limitStr)
+func (p *post) ReactPost(w http.ResponseWriter, r *http.Request) {
+	err := p.Service_React(r.Context(), r.Body)
 	if err != nil {
-		p.loger.Error.Println("Invalid limit:", err)
-		w.WriteHeader(http.StatusBadRequest)
-		json.NewEncoder(w).Encode(entity.ErrorResponse{Error: "Invalid limit"})
-		return
 	}
+}
 
-	offset, err := strconv.Atoi(offsetStr)
+func (p *post) CreatePost(w http.ResponseWriter, r *http.Request) {
+	NewFileName := uuid.NewString()
+	post := entity.Post{
+		Image: NewFileName,
+	}
+	err := p.Service_CreateOne(r.Context(), r.Body, r.Form["users"], post)
 	if err != nil {
-		p.loger.Error.Println("Invalid offset:", err)
 		w.WriteHeader(http.StatusBadRequest)
-		json.NewEncoder(w).Encode(entity.ErrorResponse{Error: "Invalid offset"})
+		json.NewEncoder(w).Encode(entity.ErrorResponse{Error: "Invalid request body"})
 		return
 	}
-
-	p.loger.Info.Println("Limit:", limit, "Offset:", offset)
-	if limit <= 0 || offset < 0 {
-		w.WriteHeader(http.StatusBadRequest)
-		json.NewEncoder(w).Encode(entity.ErrorResponse{Error: "Invalid limit or offset"})
-		return
-	}
-
-	posts, err := p.GetPostsByUserService(r.Context(), limit, offset)
+	File, FileHeader, err := r.FormFile("image")
+	err = FileUpload(NewFileName, File, FileHeader, err)
 	if err != nil {
-		p.loger.Error.Println("1z", err)
-		w.WriteHeader(http.StatusInternalServerError)
+		w.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(w).Encode(entity.ErrorResponse{Error: "File Not Uploaded"})
+		return
+	}
+}
+
+func (p *post) GetPost(w http.ResponseWriter, r *http.Request) {
+	data, err := p.Service_GetOne(r.Context(), r.FormValue("id"))
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
 		json.NewEncoder(w).Encode(entity.ErrorResponse{Error: err.Error()})
 		return
 	}
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	p.loger.Info.Println("response:", posts)
-	json.NewEncoder(w).Encode(posts)
+	w.Write(data)
 }
 
-/*             NextJs                */
-func (u *post) React(w http.ResponseWriter, r *http.Request) {
-	id := r.Context().Value(entity.ContextID).(int)
-	prep, err := u.db.PrepareContext(r.Context(), `SELECT $1 as user_id, post.id, $2 as status
-		FROM posts AS post
-		LEFT JOIN follows AS follow 
-		  ON follow.followed_id = post.user_id AND follow.follower_id = $1
-		LEFT JOIN group_members AS gm 
-		  ON gm.group_id = post.group_id AND gm.member_id = $1
-		WHERE 
-		  ((post.group_id IS NOT NULL AND gm.member_id IS NOT NULL)
-		    OR 
-		  (post.group_id IS NULL AND follow.follower_id = $1))
-		    AND post.id = $3`)
+func (p *post) GetPostByUsername(w http.ResponseWriter, r *http.Request) {
+	data, err := p.GetPostsByUserService(r.Context(), r.PathValue("username"))
 	if err != nil {
-		return
-	}
-	react := entity.Reaction{}
-	err = json.NewDecoder(r.Body).Decode(&react)
-	if err != nil {
-		return
-	}
-	_, err = prep.Exec(id, react.ID, react.Status)
-	if err != nil {
-		return
-	}
-	w.Write([]byte(`{
-		result: "done"
-	}`))
-}
-
-func (u *post) Post(w http.ResponseWriter, r *http.Request) {
-	switch r.Method {
-	case "POST":
-		u.CreatePost(w, r)
-	case "GET":
-		u.GetPost(w, r)
-	default:
-		w.WriteHeader(http.StatusMethodNotAllowed)
-	}
-}
-
-func (u *post) CreatePost(w http.ResponseWriter, r *http.Request) {
-	id := r.Context().Value(entity.ContextID).(int)
-	prep, err := u.db.PrepareContext(r.Context(), `INSERT into posts
-		SELECT $1, $2, $3, $4, $5, $6
-		FROM users AS user
-		LEFT JOIN group_members AS gm ON gm.group_id = $5 AND gm.member_id = $1
-		WHERE user.id = $1 AND $2 NOT NULL AND $3 NOT NULL AND status NOT NULL`)
-	if err != nil {
-		return
-	}
-	post := entity.Post{}
-	json.NewDecoder(r.Body).Decode(&post)
-	_, err = prep.Exec(id, post.Title, post.Content, post.Image, post.GroupID, post.Status)
-	if err != nil {
-		return
-	}
-	w.Write([]byte(`{
-		result: "done"
-	}`))
-}
-
-func (u *post) GetPost(w http.ResponseWriter, r *http.Request) {
-	prep, err := u.db.PrepareContext(r.Context(), `SELECT
-		    post.id,
-		    post.title,
-		    post.content,
-			post.image,
-			post.status,
-			post.group_id,
-		    user.nickname AS creator
-		FROM posts AS post 
-		INNER JOIN users AS user ON user.id = post.user_id
-		LEFT JOIN group_members AS gm ON gm.group_id=post.group_id AND gm.member_id = $1
-		LEFT JOIN follows AS follow ON follow.followed_id = post.user_id AND follow.follower_id = 1
-		WHERE
-		    (post.group_id IS NOT NULL AND gm.member_id = $1 AND post.id=$2)
-		    OR 
-		    (post.group_id IS NULL AND follow.follower_id = $1 AND post.id=$2);`)
-	if err != nil {
-		return
-	}
-	post := entity.Post{}
-	res := prep.QueryRowContext(r.Context(), post.ID)
-	err = res.Scan(&post.ID, &post.Title, &post.Content, &post.Image, &post.Status, &post.GroupID, &post.Nickname)
-	if err != nil {
-		return
-	}
-	data, err := json.Marshal(post)
-	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(w).Encode(entity.ErrorResponse{Error: err.Error()})
 		return
 	}
 	w.Write(data)
--- backend/internal/post/service.go
+++ backend/internal/post/service.go
@@ -1,52 +1,133 @@
 package post
 
 import (
+	"bufio"
 	"context"
+	"encoding/json"
 	"errors"
+	"fmt"
+	"io"
+	"mime/multipart"
+	"net/http"
+	"os"
+	"strconv"
+	"strings"
+
 	"socialNetwork/entity"
+
+	_ "image/gif"
+	_ "image/jpeg"
+	_ "image/png"
 )
 
-// func (p *post) CreatePostService(post entity.Post, id int) (int, int, error) {
-// 	var (
-// 		status, GroupID int
-// 		err error
-// 	)
-// 	switch post.Status {
-// 	case 0:
-// 		//repo => create group
-// 		GroupID, status, err = p.CreateGroup(post, id)
-// 		post.GroupID = GroupID
-// 		if err != nil {
-// 			return 0, status, err
-// 		}
-// 	default:
-// 		if !(post.Status == 1 || post.Status == 2) {
-// 			return 0, http.StatusBadRequest, errors.New("invalid post status")
-// 		}
-// 	}
-
-// 	// repo => create post
-// 	return p.CreatePostRepo(post, id)
-// }
-
-// get
-// 0 => table(group) contains user id
-// 1 => table(follows) contains user id
-// 2 => get direct
-
-// react
-// 0 => table(group) contains user id
-// 1 => table(follows) contains user id
-// if not status forbidden
-
-func (p *post) GetPostsByUserService(ctx context.Context, limit, offset int) ([]entity.Post, error) {
-	userID, ok := ctx.Value(entity.ContextID).(int)
-	if !ok {
-		return nil, errors.New("user id not found in context")
-	}
-	groups, err := p.GetPostsByUserID(ctx, userID, limit, offset)
+func (p *post) Service_GetAll(ctx context.Context) (data []byte, err error) {
+	id := 1 // ctx.Value(entity.ContextID).(int)
+	posts, err := p.Repo_GetAll(ctx, id)
 	if err != nil {
-		return nil, err
+		return
 	}
-	return groups, nil
+	data, err = json.Marshal(posts)
+	return
+}
+
+func (p *post) Service_GetOne(ctx context.Context, post_str string) (data []byte, err error) {
+	id := 1 // ctx.Value(entity.ContextID).(int)
+	post_id, err := strconv.Atoi(post_str)
+	if err != nil {
+		return
+	}
+	if p.Repo_UserCanPost(ctx, id, post_id) {
+		var post entity.Post
+		post, err = p.Repo_GetOne(ctx, post_id)
+		if err != nil {
+			return
+		}
+		data, err = json.Marshal(post)
+		return
+	} else {
+		err = errors.New("you can't see post")
+		return
+	}
+}
+
+func (p *post) Service_CreateOne(ctx context.Context, body io.ReadCloser, users []string, post entity.Post) error {
+	id := 1 // ctx.Value(entity.ContextID).(int)
+	ImageFileName := post.Image
+	json.NewDecoder(body).Decode(&post)
+	post.Image = ImageFileName
+	err := post.Validate(users)
+	if err != nil {
+		return errors.New(string("{ error: " + err.Error() + " }"))
+	}
+	p.Repo_CreatePost(ctx, id, post)
+	return nil
+}
+
+func (p *post) Service_React(ctx context.Context, body io.ReadCloser) (err error) {
+	id := 1 // ctx.Value(entity.ContextID).(int)
+	react := entity.Vote{}
+	json.NewDecoder(body).Decode(&react)
+	if p.Repo_UserCanPost(ctx, id, react.ID) {
+		err = p.Repo_React(ctx, id, react)
+	} else {
+		err = errors.New("you can't see the post")
+	}
+	return
+}
+
+func (p *post) GetPostsByUserService(ctx context.Context, username string) (data []byte, err error) {
+	userID := 1 // ctx.Value(entity.ContextID).(int)
+	posts, err := p.GetPostsByUserID(ctx, userID, username)
+	if err != nil {
+		return
+	}
+	data, err = json.Marshal(posts)
+	if err != nil {
+		return
+	}
+	return
+}
+
+func FileUpload(NeWFileName string, File multipart.File, FileHeader *multipart.FileHeader, err error) error {
+	if err != nil {
+		return err
+	}
+	fmt.Println((FileHeader.Size / 1024))
+	var FileContent []byte
+	var FileIsAccepted bool
+	AcceptedTypes := []string{"png", "gif", "jpg", "jpeg"}
+	for _, Type := range AcceptedTypes {
+		if strings.HasSuffix(FileHeader.Filename, Type) {
+			FileIsAccepted = true
+		}
+	}
+	if !FileIsAccepted {
+		return errors.New("file type is not accepted")
+	}
+	switch FileHeader.Filename[len(FileHeader.Filename)-3:] {
+	case "gif":
+		if (FileHeader.Size/1024) > 1024 || (FileHeader.Size/1024) < 10 {
+			return errors.New("file size unmatched our conditions")
+		}
+	default:
+		if (FileHeader.Size/1024) > 500 || (FileHeader.Size/1024) < 10 {
+			return errors.New("file size unmatched our conditions")
+		}
+	}
+	FileIsAccepted = false
+	reader := bufio.NewReader(File)
+	FileContent, _ = io.ReadAll(reader)
+	AcceptedTypes = []string{"image/png", "image/jpg", "image/jpeg", "image/gif"}
+	FileType := http.DetectContentType((FileContent))
+	for _, Type := range AcceptedTypes {
+		if Type == FileType {
+			FileIsAccepted = true
+		}
+		fmt.Println(FileIsAccepted, " : ", Type, " == ", FileType)
+	}
+	if !FileIsAccepted {
+		return errors.New("file type not matched")
+	}
+	os.WriteFile("../"+NeWFileName+".png", FileContent, 0o444)
+	return nil
 }
