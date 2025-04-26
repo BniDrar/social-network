460ce9b90b20836b3b999029f8bbca69e8cd6f07
ielharra <moroccobrahem@gmail.com>
2025-04-27 00:47:39 +0100

    add the comments handlers to the router

--- backend/internal/server/router.go
+++ backend/internal/server/router.go
@@ -121,26 +121,39 @@ func (app *App) createRoutes() []Route {
 			handler: app.GetPosts,
 			Role:    User,
 		},
+		//comments handlers
+		{
+			Path: "/api/comment/add",
+			handler: app.AddComment,
+			Role: User,
+		},
+		{
+			Path: "api/comment/get",
+			handler: app.GetComments,
+			Role: User,
+		},
+		//ws handlers
 		{
 			Path:    "/api/ws",
 			handler: app.WebSocket,
 			Role:    User,
 		},
-		// {
-		// 	Path:    "/api/event/create",
-		// 	handler: app.CreateEvent,
-		// 	Role:    User,
-		// },
-		// {
-		// 	Path:    "/api/event/vote",
-		// 	handler: app.VoteEvent,
-		// 	Role:    User,
-		// },
-		// {
-		// 	Path:    "/api/event/get",
-		// 	handler: app.GetEvent,
-		// 	Role:    User,
-		// },
+		//events handlers
+		{
+			Path:    "/api/event/create",
+			handler: app.CreateEvent,
+			Role:    User,
+		},
+		{
+			Path:    "/api/event/vote",
+			handler: app.VoteEvent,
+			Role:    User,
+		},
+		{
+			Path:    "/api/event/get",
+			handler: app.GetEvent,
+			Role:    User,
+		},
 		/*... handlers*/
 	}
 }
