2486621a0ab8a8b3f7f357b600e283e7b7463b16
brahem01 <moroccobrahem@gmail.com>
2025-04-30 19:49:03 +0100

    solve the problem with getting embeded field in profile response

--- backend/entity/user.go
+++ backend/entity/user.go
@@ -1,22 +1,20 @@
 package entity
 
-import "database/sql"
-
 type User struct {
-	ID             uint           `json:"id,omitempty"`
-	Nickname       sql.NullString `json:"nickname,omitempty"`
-	Email          string         `json:"email,omitempty"`
-	Password       string         `json:"password,omitempty"`
-	Avatar         NullString     `json:"avatar,omitempty"`
-	First          string         `json:"first,omitempty"`
-	Last           string         `json:"last,omitempty"`
-	DateOfBirth    string         `json:"birthday,omitempty"`
-	AboutMe        string         `json:"about_me,omitempty"`
-	Status         uint           `json:"status,omitempty"`
-	ProfileOwner   bool           `json:"profile_owner,omitempty"`
-	FollowingState uint           `json:"following,omitempty"` // 1 follower, 2 following, 0 none
-	FollowersCount uint           `json:"followers_count,omitempty"`
-	FollowingCount uint           `json:"following_count,omitempty"`
+	ID             uint       `json:"id,omitempty"`
+	Nickname       NullString `json:"nickname,omitempty"`
+	Email          string     `json:"email,omitempty"`
+	Password       string     `json:"password,omitempty"`
+	Avatar         NullString `json:"avatar,omitempty"`
+	First          string     `json:"first,omitempty"`
+	Last           string     `json:"last,omitempty"`
+	DateOfBirth    string     `json:"birthday,omitempty"`
+	AboutMe        string     `json:"about_me,omitempty"`
+	Status         uint       `json:"status,omitempty"`
+	ProfileOwner   bool       `json:"profile_owner,omitempty"`
+	FollowingState uint       `json:"following,omitempty"` // 1 follower, 2 following, 0 none
+	FollowersCount uint       `json:"followers_count,omitempty"`
+	FollowingCount uint       `json:"following_count,omitempty"`
 }
 
 type Follows struct {
