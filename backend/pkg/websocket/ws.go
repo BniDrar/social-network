83a5182e95f1422577ecb4bf96d596ecf3d12098
OUSSAMABENALI <oussama.benali@ump.ac.ma>
2025-05-01 19:58:38 +0100

    able to broad cast now

--- backend/pkg/websocket/ws.go
+++ backend/pkg/websocket/ws.go
@@ -39,8 +39,8 @@ func (m *WsManager) AddClient(userID uint, conn *websocket.Conn) {
 		fmt.Printf("online ---------------> id  %d ", i)
 	}
 	log.Println("after adding", len(m.Clients))
-	// go m.readMessage(userID)
-	// go m.writeMessage(userID)
+	go m.readMessage(userID)
+	go m.writeMessage(userID)
 }
 
 func (m *WsManager) RemoveClient(userID uint) {
@@ -124,13 +124,14 @@ func (m *WsManager) Broadcast(userID uint, message []byte) {
 	defer m.Mtx.Unlock()
 	for id, c := range m.Clients {
 		log.Printf("brod cast to %d", id)
-		if id != userID {
-			select {
-			case c.send <- message:
-			default:
-				log.Printf("Client %d send buffer is full", id)
-			}
+		// if id != userID {
+		fmt.Println("valid valid valid")
+		select {
+		case c.send <- message:
+		default:
+			log.Printf("Client %d send buffer is full", id)
 		}
+		// }
 	}
 }
 
