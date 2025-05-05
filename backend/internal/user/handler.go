8abdd03cfc388fda6c09c23a809e4b59be95cde9
ielharra <moroccobrahem@gmail.com>
2025-05-05 06:52:16 +0100

    remove unnecessary print

--- backend/internal/user/handler.go
+++ backend/internal/user/handler.go
@@ -4,7 +4,6 @@ import (
 	"database/sql"
 	"encoding/json"
 	"errors"
-	"log"
 	"net/http"
 	"os"
 	"strconv"
@@ -154,7 +153,6 @@ func (u *user) Login(w http.ResponseWriter, r *http.Request) {
 		json.NewEncoder(w).Encode(entity.ErrorResponse{Error: err.Error()})
 		return
 	}
-	log.Println(User)
 	id, err := u.authenticateService(User.Username, User.Password)
 	if err != nil {
 		if errors.Is(err, config.ErrInvalidCredentials) {
