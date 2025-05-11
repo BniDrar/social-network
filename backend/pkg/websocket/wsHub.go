9bfbb6000b0ead67efaafe3a0e9e0da05030411d
ielharra <moroccobrahem@gmail.com>
2025-05-11 23:43:42 +0100

    add a case of log out and the ws is not establishing yet to not crash the code

--- backend/pkg/websocket/wsHub.go
+++ backend/pkg/websocket/wsHub.go
@@ -134,7 +134,6 @@ func (h *Hub) Run() {
 			log.Printf("Client %d registered", client.userID)
 
 		case client := <-h.unregister:
-			log.Println("here")
 			h.mu.Lock()
 			if _, ok := h.Clients[client.userID]; ok {
 				delete(h.Clients, client.userID)
@@ -266,5 +265,7 @@ func (h *Hub) IsOnline(userId uint) bool {
 }
 
 func (h *Hub) Unregister(userID uint) {
-	h.unregister <- h.Clients[userID]
+	if h.IsOnline(userID) {
+		h.unregister <- h.Clients[userID]
+	}
 }
