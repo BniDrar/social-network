71162fbe062f748bbdb335faf36dff6039d97108
obenali <oussama.benali@ump.ac.ma>
2025-05-28 12:57:54 +0100

    fix duplication in the posts in the profile page

--- frontend/src/components/posts/posts.jsx
+++ frontend/src/components/posts/posts.jsx
@@ -3,30 +3,46 @@ import { useEffect, useState } from "react";
 import styles from "./posts.module.css"
 import Post from "../post/post";
 import { getPosts } from "@/services/posts";
+import { usePathname } from "next/navigation";
 
 const Posts = ({ body, lien }) => {
     const [posts, setPosts] = useState([]);
     const [last_id, setLastId] = useState(null);
     const [loading, setLoading] = useState(false);
     const [hasMore, setHasMore] = useState(true);
+    const pathname = usePathname();
+
 
     const fetchPosts = async () => {
         if (loading || !hasMore) return;
         setLoading(true);
+
+        const body = {};
         if (posts.length > 0) {
             const lastPost = posts[posts.length - 1];
             body.last_id = lastPost.id;
             body.creation_time = lastPost.created_at;
         }
-        body.limit = 10
+        body.limit = 10;
+
         try {
-            const fetchedPosts = await getPosts(body, lien);
+            const fetchedPosts = await getPosts(
+                body,
+                `${process.env.BACKEND_URL}/api/posts`
+            );
 
             if (!fetchedPosts || fetchedPosts.length === 0) {
                 setHasMore(false);
             } else {
-                const postsArray = Array.isArray(fetchedPosts) ? fetchedPosts : [];
-                setPosts((prev) => prev.length > 0 ? [...prev, ...postsArray] : [...postsArray]);
+                setPosts((prev) => {
+                    const existingIds = prev ? new Set(prev.map((p) => p.id)) : new Set();
+                    if (fetchedPosts.length > 0) {
+                        const newUniquePosts = fetchedPosts.filter(
+                            (p) => !existingIds.has(p.id)
+                        );
+                        return [...prev, ...newUniquePosts];
+                    }
+                });
             }
         } catch (error) {
             console.error("Error fetching posts:", error);
@@ -36,8 +52,10 @@ const Posts = ({ body, lien }) => {
     };
 
     useEffect(() => {
+        setPosts([]);
+        setHasMore(true);
         fetchPosts();
-    }, []);
+    }, [pathname]);
 
     useEffect(() => {
         let debounceTimer;
